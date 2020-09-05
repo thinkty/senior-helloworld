d9a90f476765e5ace5151087d9c8ac8c71e12359
thinkty <thinkty@naver.com>
2020-09-05 17:05:40 +0900

    Modified footer to be more expandable

--- src/Footer/index.js
+++ src/Footer/index.js
@@ -9,21 +9,41 @@ import { Link } from 'react-router-dom';
  * - Leave a feedback: /feedback
  */
 export default function Footer() {
+  const width = window.innerWidth;
+  const fontSize = width > 1000 ? 15 : 10;
+  const links = [
+    {
+      name: "Github",
+      url: "https://github.com/thinkty/senior-helloworld",
+      local: false,
+    },
+    {
+      name: "가이드 작성하기",
+      url: "/post",
+      local: true,
+    },
+    {
+      name: "문의하기",
+      url: "/feedback",
+      local: true,
+    },
+  ];
+
   return (
-    <Space>
-      <a href="https://github.com/thinkty/senior-helloworld"> Github </a>
-      <i className="fa fa-github"></i>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <Link to="/post"> 가이드 작성하기 </Link>
-      <i className="fa fa-edit"></i>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <Link to="/feeback"> 문의하기 </Link>
+    <Space size="large">
+      {
+        links.map(({ name, url, local }) => (
+          local
+          ?
+          <Link to={url} key={name}>
+            <p style={{ fontSize }}> {name} </p>
+          </Link> 
+          :
+          <a href={url} key={name}>
+            <p style={{ fontSize }}> {name} </p>
+          </a>
+        ))
+      }
     </Space>
   );
 }
