3634dc9be75d3ae604fb80bd0733a49b1c42ca5c
thinkty <thinkty@naver.com>
2020-09-05 18:45:09 +0900

    Added page to select posting method

--- src/CreatePage/index.js
+++ src/CreatePage/index.js
@@ -1,31 +1,79 @@
-import React from 'react';
-import { Layout } from 'antd';
-import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import { Layout, Space, Button } from 'antd';
+import { Link } from 'react-router-dom';
 import AttachFilePage from './AttachFilePage';
 import EditorPage from './EditorPage';
 
 const attachFileMethod = "파일 첨부하기";
-const useEditorMethod = "작성하기"
-export const methods = [attachFileMethod, useEditorMethod];
+const useEditorMethod = "에디터로 작성하기"
+const methods = [attachFileMethod, useEditorMethod];
 
-export default function CreatePage(props) {
-  const { method } = props;
-
-  if (method === attachFileMethod) {
-    return <AttachFilePage />;
-  } else if (method === useEditorMethod) {
-    return <EditorPage />;
+/**
+ * This page allows user to upload a markdown file or use the editor to create a
+ * guide.
+ */
+export default class CreatePage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { selectedMethod: '' };
   }
 
-  return (
-    <Layout style={{ height: "100vh" }}>
-      <Layout.Content>
+  /**
+   * Method called when user selects a method
+   */
+  selectMethod = (event) => {
+    // I am using innertext instead of id as sometimes, the text gets pressed
+    // instead of the button. It might be a bug in the framework but not sure if
+    // it is intended.
+    this.setState({ selectedMethod: event.target.innerText });
+  }
 
-      </Layout.Content>
-    </Layout>
-  );
-}
+  /**
+   * Method to cancel the currently selected method
+   */
+  onCancel = () => {
+    this.setState({ selectedMethod: '' });
+  }
 
-CreatePage.propTypes = {
-  method: PropTypes.string.isRequired,
+  render() {
+    const { selectedMethod } = this.state;
+
+    if (selectedMethod === attachFileMethod) {
+      return <AttachFilePage onCancel={this.onCancel} />;
+    } else if (selectedMethod === useEditorMethod) {
+      return <EditorPage onCancel={this.onCancel} />;
+    }
+  
+    return (
+      <Layout style={{ height: "100vh" }}>
+        <Layout.Content>
+          <Space
+            align="center"
+            size="large"
+            direction="vertical"
+            style={{ width: "100%", marginTop: "45vh" }}
+          >
+            <h1> 가이드 작성 방법 선택하기 </h1>
+            <Space align="center" size="large">
+              <Link to="/">
+                <Button size="large"> 작성 취소 </Button>
+              </Link>
+              {
+                methods.map((method) => (
+                  <Button
+                    key={method}
+                    onClick={this.selectMethod}
+                    size="large"
+                    type="primary"
+                  >
+                    {method}
+                  </Button>
+                ))
+              }
+            </Space>
+          </Space>
+        </Layout.Content>
+      </Layout>
+    );
+  }
 }
