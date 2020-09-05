add758bdf46beacd85e37ec7e0df2c19a7f9de6d
thinkty <thinkty@naver.com>
2020-09-05 20:10:31 +0900

    Editor page now showing input field and resulting md

---
+++ src/util/guide.js
@@ -0,0 +1,12 @@
+/**
+ * Module with helper functions related to guides
+ */
+
+/**
+ * Function to send the guide to the server to be reviewed
+ *
+ * @param {string} content Markdown content in text
+ */
+export async function sendGuide(content) {
+
+}
